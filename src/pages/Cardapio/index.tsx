e85a0d0eeaa64c3fdd51bb716d647c33fd20ba31
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-13 20:29:42 -0300

    adicionado a logica dos filtros

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -36,7 +36,11 @@ export default function Cardapio() {
             setOrdenador={setOrdenador} 
           />
         </div>
-        <Itens />
+        <Itens 
+          busca={busca} 
+          filtro={filtro} 
+          ordenador={ordenador} 
+        />
       </section>
     </main>
   )
