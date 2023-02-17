48b9a81c4836f306708a109cdce5baf5afa407fc
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-17 16:09:09 -0300

    projeto finalizado

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,17 +2,19 @@ import styles from './Prato.module.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
   if (!prato) {
-    return '';
+    return <NotFound />;
   }
 
   return (
-    <>
+    <PaginaPadrao>
       <button 
         className={styles.voltar}
         onClick={() => navigate(-1)}
@@ -33,6 +35,6 @@ export default function Prato() {
           <TagsPrato {...prato} />
         </div>
       </section>
-    </>
+    </PaginaPadrao>
   );
 }
