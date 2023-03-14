a61618bb2888067509a6edabba061afaf6f61f19
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-03-14 10:35:51 -0300

    usado useMemo para o icone do buscador

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import styles from './Buscador.module.scss';
 import { CgSearch } from 'react-icons/cg';
 
@@ -8,17 +8,15 @@ interface Props {
 } 
 
 function Buscador({ busca, setBusca }: Props) {
+  const elemento = useMemo(() => <CgSearch size={30} color="#4c4d54" />, []);
   return (
     <div className={styles.buscador}>
       <input 
         value={busca}
         onChange={evento => setBusca(evento.target.value)}
         placeholder="Buscar"
-      />
-      <CgSearch 
-        size={30}
-        color="#4c4d54"
-      />
+      /> 
+      {elemento}
     </div>
   );
 }
