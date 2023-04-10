2b6e1cf52c696530caad8df234d942493c3a210f
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-04-09 23:24:15 -0300

    alterado o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <p>Desenvolvido por <a href="https://lucasrodrigues.vercel.app/">Lucas Rodrigues</a></p>
       <Logo />
     </footer>
   );
